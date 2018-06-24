2437f830676c6fc863d14b232dfdec6cf231da9e
gnlsw <today_wsl@163.com>
2018-06-24 18:07:08 +0800

    tab转为空格

--- AlgContainer/alg_container.c
+++ AlgContainer/alg_container.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 typedef unsigned int    VOS_UINT32;
-typedef char   			VOS_CHAR;
+typedef char            VOS_CHAR;
 typedef unsigned char   VOS_UINT8;
 typedef void            VOS_VOID;
 
@@ -24,16 +24,16 @@ typedef void            VOS_VOID;
 #define VOS_MemCopy(dst, src, num)      memcopy(dst, src, num)
 
 typedef enum ENUM_ITEM_STATUS {
-	ENUM_ITEM_IDLE = 0,
-	ENUM_ITEM_USED,
+    ENUM_ITEM_IDLE = 0,
+    ENUM_ITEM_USED,
 
-	ENUM_ITEM_BUTT
+    ENUM_ITEM_BUTT
 }ENUM_ITEM_STATUS;
 
 typedef struct CONTROL_ITEM_STRU {
-	ENUM_ITEM_STATUS enStatus;
-    VOS_UINT32  	udwPrev;
-    VOS_UINT32  	udwNext;
+    ENUM_ITEM_STATUS enStatus;
+    VOS_UINT32      udwPrev;
+    VOS_UINT32      udwNext;
 } CONTROL_ITEM;
 
 typedef struct DATA_ITEM_STRU {
@@ -68,17 +68,17 @@ int main()
     udwRetCode = InitAlgContainer(&g_stAlgContainer, MAX_ITEM_NUM, sizeof(DATA_ITEM));
     if(VOS_OK != udwRetCode)
     {
-    	printf("fail to init alg container\r\n");
-    	return 0;
+        printf("fail to init alg container\r\n");
+        return 0;
     }
 
     PrintAlgContainer(&g_stAlgContainer);
 
     for(udwCount = 0; udwCount < MAX_ITEM_NUM; udwCount++)
     {
-    	udwIndex = AllocAlgContainerItem(&g_stAlgContainer);
-    	printf("new item, index = %d\n", udwIndex);
-    	PrintAlgContainer(&g_stAlgContainer);
+        udwIndex = AllocAlgContainerItem(&g_stAlgContainer);
+        printf("new item, index = %d\n", udwIndex);
+        PrintAlgContainer(&g_stAlgContainer);
     }
 
     FreeAlgContainerItem(&g_stAlgContainer, 5);
@@ -103,24 +103,24 @@ VOS_UINT32 InitAlgContainer(ALG_CONTAINER *pstAlgContainer, VOS_UINT32 udwNum, V
     pstControlItem = (CONTROL_ITEM *)pBuffer;
 
     /* 初始化时构建空闲链 */
-    pstControlItem[0].udwPrev 	= VOS_NULL_DWORD;
-    pstControlItem[0].udwNext 	= 1;
-    pstControlItem[0].enStatus 	= ENUM_ITEM_IDLE;
+    pstControlItem[0].udwPrev   = VOS_NULL_DWORD;
+    pstControlItem[0].udwNext   = 1;
+    pstControlItem[0].enStatus  = ENUM_ITEM_IDLE;
     for(udwIndex = 1; udwIndex < udwNum - 1; udwIndex++)
     {
-    	pstControlItem[udwIndex].enStatus = ENUM_ITEM_IDLE;
+        pstControlItem[udwIndex].enStatus = ENUM_ITEM_IDLE;
         pstControlItem[udwIndex].udwPrev = udwIndex - 1;
         pstControlItem[udwIndex].udwNext = udwIndex + 1;
     }
-    pstControlItem[udwIndex].udwPrev 	= udwIndex - 1;
-    pstControlItem[udwIndex].udwNext 	= VOS_NULL_DWORD;
-    pstControlItem[udwIndex].enStatus 	= ENUM_ITEM_IDLE;
+    pstControlItem[udwIndex].udwPrev    = udwIndex - 1;
+    pstControlItem[udwIndex].udwNext    = VOS_NULL_DWORD;
+    pstControlItem[udwIndex].enStatus   = ENUM_ITEM_IDLE;
 
     pstAlgContainer->udwHead           = 0;
     pstAlgContainer->udwTail           = udwNum - 1;
     pstAlgContainer->udwMaxNum         = udwNum;
     pstAlgContainer->udwUsedNum        = 0;
-    pstAlgContainer->udwItemSize   	= udwDataSize;
+    pstAlgContainer->udwItemSize    = udwDataSize;
     pstAlgContainer->pstControlItem    = pstControlItem;
     pstAlgContainer->pstDataBaseAddr   = pstControlItem + udwNum;
 
@@ -130,7 +130,7 @@ VOS_UINT32 InitAlgContainer(ALG_CONTAINER *pstAlgContainer, VOS_UINT32 udwNum, V
 VOS_UINT32 AllocAlgContainerItem(ALG_CONTAINER *pstAlgContainer)
 {
     CONTROL_ITEM    *pstControlItem = VOS_NULL_PTR;
-    VOS_UINT32 		udwIndex = VOS_NULL_DWORD;
+    VOS_UINT32      udwIndex = VOS_NULL_DWORD;
 
     if(VOS_NULL_PTR == pstAlgContainer)
     {
@@ -147,8 +147,8 @@ VOS_UINT32 AllocAlgContainerItem(ALG_CONTAINER *pstAlgContainer)
 
     if(pstControlItem[udwIndex].udwNext < pstAlgContainer->udwMaxNum)
     {
-		pstAlgContainer->udwHead = pstControlItem[udwIndex].udwNext;
-		pstControlItem[pstAlgContainer->udwHead].udwPrev = VOS_NULL_DWORD;
+        pstAlgContainer->udwHead = pstControlItem[udwIndex].udwNext;
+        pstControlItem[pstAlgContainer->udwHead].udwPrev = VOS_NULL_DWORD;
     }
     else
     {
@@ -157,8 +157,8 @@ VOS_UINT32 AllocAlgContainerItem(ALG_CONTAINER *pstAlgContainer)
     }
 
     pstControlItem[udwIndex].udwPrev = VOS_NULL_DWORD;
-	pstControlItem[udwIndex].udwNext = VOS_NULL_DWORD;
-	pstControlItem[udwIndex].enStatus = ENUM_ITEM_USED;
+    pstControlItem[udwIndex].udwNext = VOS_NULL_DWORD;
+    pstControlItem[udwIndex].enStatus = ENUM_ITEM_USED;
     pstAlgContainer->udwUsedNum++;
 
     return udwIndex;
@@ -189,10 +189,10 @@ VOS_UINT32 FreeAlgContainerItem(ALG_CONTAINER *pstAlgContainer, VOS_UINT32 udwIn
     }
     else
     {
-    	pstControlItem[udwIndex].udwPrev = pstAlgContainer->udwTail;
-    	pstControlItem[udwIndex].udwNext = VOS_NULL_DWORD;
-		pstControlItem[pstAlgContainer->udwTail].udwNext = udwIndex;
-		pstAlgContainer->udwTail = udwIndex;
+        pstControlItem[udwIndex].udwPrev = pstAlgContainer->udwTail;
+        pstControlItem[udwIndex].udwNext = VOS_NULL_DWORD;
+        pstControlItem[pstAlgContainer->udwTail].udwNext = udwIndex;
+        pstAlgContainer->udwTail = udwIndex;
     }
 
     pstControlItem[udwIndex].enStatus = ENUM_ITEM_IDLE;
@@ -203,41 +203,41 @@ VOS_UINT32 FreeAlgContainerItem(ALG_CONTAINER *pstAlgContainer, VOS_UINT32 udwIn
 
 VOS_VOID PrintAlgContainer(ALG_CONTAINER *pstAlgContainer)
 {
-	CONTROL_ITEM    	*pstControlItem = VOS_NULL_PTR;
-	VOS_UINT32      	udwIndex = VOS_NULL_DWORD;
-	ENUM_ITEM_STATUS	enStatus = ENUM_ITEM_BUTT;
-	VOS_CHAR        	*apucStatusStr[ENUM_ITEM_BUTT + 1] = {"idle", "used", "null"};
+    CONTROL_ITEM        *pstControlItem = VOS_NULL_PTR;
+    VOS_UINT32          udwIndex = VOS_NULL_DWORD;
+    ENUM_ITEM_STATUS    enStatus = ENUM_ITEM_BUTT;
+    VOS_CHAR            *apucStatusStr[ENUM_ITEM_BUTT + 1] = {"idle", "used", "null"};
 
-	printf("----------------------------------------------\n");
-	printf("udwMaxNum  = %-10u, udwUsedNum = %-10u\n", pstAlgContainer->udwMaxNum, pstAlgContainer->udwUsedNum);
-	printf("udwHead    = %-10u, udwTail    = %-10u\n", pstAlgContainer->udwHead, pstAlgContainer->udwTail);
+    printf("----------------------------------------------\n");
+    printf("udwMaxNum  = %-10u, udwUsedNum = %-10u\n", pstAlgContainer->udwMaxNum, pstAlgContainer->udwUsedNum);
+    printf("udwHead    = %-10u, udwTail    = %-10u\n", pstAlgContainer->udwHead, pstAlgContainer->udwTail);
 
-	printf("---begin idle link---\n");
-	printf("%-10s | %-10s | %-10s | %s\n", "status", "index", "prev", "next");
-	pstControlItem = pstAlgContainer->pstControlItem;
-	udwIndex = pstAlgContainer->udwHead;
-	while(udwIndex < pstAlgContainer->udwMaxNum)
-	{
-		enStatus = pstControlItem[udwIndex].enStatus;
-		printf("%-10s | %-10u | ", apucStatusStr[enStatus], udwIndex);
-		printf("%-10u | %u\n", pstControlItem[udwIndex].udwPrev, pstControlItem[udwIndex].udwNext);
-		udwIndex = pstControlItem[udwIndex].udwNext;
-	}
-	printf("---end idle link---\n");
-	printf("---begin used link---\n");
-	printf("%-10s | %-10s | %-10s | %s\n", "status", "index", "prev", "next");
-	for(udwIndex = 0; udwIndex < pstAlgContainer->udwMaxNum; udwIndex++)
-	{
-		enStatus = pstControlItem[udwIndex].enStatus;
-		if(ENUM_ITEM_IDLE == enStatus)
-		{
-			continue;
-		}
-			printf("%-10s | %-10u | ", apucStatusStr[enStatus], udwIndex);
-			printf("%-10u | %u\n", pstControlItem[udwIndex].udwPrev, pstControlItem[udwIndex].udwNext);
-	}
-	printf("---end used link---\n");
-	printf("----------------------------------------------\n");
+    printf("---begin idle link---\n");
+    printf("%-10s | %-10s | %-10s | %s\n", "status", "index", "prev", "next");
+    pstControlItem = pstAlgContainer->pstControlItem;
+    udwIndex = pstAlgContainer->udwHead;
+    while(udwIndex < pstAlgContainer->udwMaxNum)
+    {
+        enStatus = pstControlItem[udwIndex].enStatus;
+        printf("%-10s | %-10u | ", apucStatusStr[enStatus], udwIndex);
+        printf("%-10u | %u\n", pstControlItem[udwIndex].udwPrev, pstControlItem[udwIndex].udwNext);
+        udwIndex = pstControlItem[udwIndex].udwNext;
+    }
+    printf("---end idle link---\n");
+    printf("---begin used link---\n");
+    printf("%-10s | %-10s | %-10s | %s\n", "status", "index", "prev", "next");
+    for(udwIndex = 0; udwIndex < pstAlgContainer->udwMaxNum; udwIndex++)
+    {
+        enStatus = pstControlItem[udwIndex].enStatus;
+        if(ENUM_ITEM_IDLE == enStatus)
+        {
+            continue;
+        }
+            printf("%-10s | %-10u | ", apucStatusStr[enStatus], udwIndex);
+            printf("%-10u | %u\n", pstControlItem[udwIndex].udwPrev, pstControlItem[udwIndex].udwNext);
+    }
+    printf("---end used link---\n");
+    printf("----------------------------------------------\n");
 
-	return;
+    return;
 }
